bc8d230febf83e3860c57e27afb1a722dda36df0
Jack Ceballos <jackrceballos@gmail.com>
2021-03-02 22:30:38 -0500

    Working on the server js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+const db = require("./models");
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.PORT || "mongodb://localhost/workout", {useNewUrlParser: true});
