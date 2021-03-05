3840b755f61cf6038083279e2de4263a4b89c334
Jack Ceballos <jackrceballos@gmail.com>
2021-03-04 19:05:27 -0500

    Fixed the mongoose connection on the server

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.PORT || "mongodb://localhost/workout", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
