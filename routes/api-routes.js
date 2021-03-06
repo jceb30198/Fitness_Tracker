0103f2ca895eb3ac32436af4bc32eb21104d2c25
Jack Ceballos <jackrceballos@gmail.com>
2021-03-06 13:19:52 -0500

    Working on API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
-const Workout = require("../models/workouts");
+const Workouts = require("../models/workouts");
+
+router.post("/api/workouts", ({body}, res) => {
+    Workouts.create(body)
+    .then((dbExercise) => {
+        res.json(dbExercise);
+        console.log(dbExercise);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
+})
 
 module.exports = router;
