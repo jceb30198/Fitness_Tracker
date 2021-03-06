52508bd1f21f93a469da01972eab5003ac35698d
Jack Ceballos <jackrceballos@gmail.com>
2021-03-06 11:00:28 -0500

    Working on the schema

--- models/workouts.js
+++ models/workouts.js
@@ -1,7 +1,32 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-
-})
+const exerciseSchema = new Schema({
+    exercise: [{
+        type: {
+            type: String,
+            trim: true,
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Please enter a name."
+        },
+        weight: {
+            type: Number,
+            required: "Please enter the weight in LBS."
+        },
+        sets: {
+            type: Number,
+            required: "Please enter the amount of sets."
+        },
+        reps: {
+            type: Number,
+            required: "Please enter the amount of reps."
+        },
+        duration: {
+            type: Number,
+            required: "Please enter the amount in minutes."
+        }
+    }]
+});
