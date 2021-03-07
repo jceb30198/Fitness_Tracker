6191a1394449dccdc9d775a98a8c4b179e53b6d5
Jack Ceballos <jackrceballos@gmail.com>
2021-03-07 13:44:23 -0500

    Connecting to Atlas

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,12 @@
 let mongoose = require("mongoose");
 let db = require("../models/workouts");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
+
 });
 
 let workoutSeed = [
